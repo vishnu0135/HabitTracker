027295a92d1373383bcb8a1c22ee64c73dd852ce
vishnu0135 <vvr.vishnu065@gmail.com>
2025-11-08 01:21:43 +0530

    Added Signup Screen with fields

---
+++ app/src/main/java/vishnu/project/habittracker/AccountRegisterActivity.kt
@@ -0,0 +1,13 @@
+package vishnu.project.habittracker
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AccountRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/vishnu/project/habittracker/EnterAppActivity.kt
+++ app/src/main/java/vishnu/project/habittracker/EnterAppActivity.kt
@@ -1,6 +1,5 @@
 package vishnu.project.habittracker
 
-
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
