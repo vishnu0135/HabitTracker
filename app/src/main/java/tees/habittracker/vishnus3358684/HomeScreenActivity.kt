4cf724f05ce2f1f1b6da11a5c1ffad590a81497c
vishnu0135 <vvr.vishnu065@gmail.com>
2025-11-16 22:36:42 +0530

    Designed Home Screen with options to add habits, view weekly and monthly habits chart

---
+++ app/src/main/java/tees/habittracker/vishnus3358684/HomeScreenActivity.kt
@@ -0,0 +1,13 @@
+package tees.habittracker.vishnus3358684
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
